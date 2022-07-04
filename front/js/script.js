dd5875c9f970809a7be0bde235f684ffbd360434
Jozef S <franck.vukelic@gmail.com>
2022-07-04 18:45:28 +0200

    Add : Loop foreach, index.html every couchs are displayed

--- front/js/script.js
+++ front/js/script.js
@@ -3,10 +3,7 @@
 
 fetch("http://localhost:3000/api/products")
     .then((response) => response.json())
-    .then((data) => {
-        // console.log(data)
-        return getProducts(data)
-    })
+    .then((data) => displayProducts(data))
 
 // 1. Le script "fetch" des données
 // 2. Ces données sont converties en .json
@@ -15,35 +12,36 @@ fetch("http://localhost:3000/api/products")
 
 //! Créer les éléments HTML depuis le script JS
 
-function getProducts(data) {
-    const _id = data[0]._id
-    const imageUrl = data[0].imageUrl
-    const altTxt = data[0].altTxt
-    const name = data[0].name
-    const description = data[0].description
-    // const {_id, imageUrl, altTxt, name, description} = data[0]
+function displayProducts(data) {
 
-    const anchor = makeAnchor(_id)
-    const article = document.createElement("article")
-    const image = makeImage(imageUrl, altTxt)
-    const title = makeTitle(name)
-    const p = makeParagraph(description)
-
-    addElementsToArticle(article, image, title, p)
-    addArticleToAnchor(anchor, article)
+    data.forEach((couch) => {        
+        
+        const _id = couch._id
+        const imageUrl = couch.imageUrl
+        const altTxt = couch.altTxt
+        const name = couch.name
+        const description = couch.description
+        // const {_id, imageUrl, altTxt, name, description} = couch
+        
+        const anchor = makeAnchor(_id)
+        const article = document.createElement("article")
+        const image = makeImage(imageUrl, altTxt)
+        const title = makeTitle(name)
+        const p = makeParagraph(description)
+        
+        addElementsToArticle(article, [image, title, p])
+        addArticleToAnchor(anchor, article)
+    })
 }
 
-// 4. La fonction "getProducts" récupère les données précédemment transmises
-// 5. Avec ces données, la fonction appelle l'imageUrl du premier élément (0) (constante)
-// 6. "getProducts" appelle/exécute la fonction "createAnchor" et met le résultat dans la constante "anchor"
-// 7. La constante "anchor" et ensuite passée à "appendChildren(anchor)" qui s'exécute 
-// 8. La fonction "getProducts", appelle la fonction "appendChildren" avec le paramètre (anchor)
 
-
-function addElementsToArticle(article, image, title, p) {
-    article.appendChild(image)
-    article.appendChild(title)
-    article.appendChild(p)
+function addElementsToArticle(article, array) {
+    array.forEach((item) => {
+        article.appendChild(item)
+    })
+    // article.appendChild(image)
+    // article.appendChild(title)
+    // article.appendChild(p)
 }
 
 function makeAnchor(id) {
